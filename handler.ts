3035a0f77477f392c95aa02aa86ee6dd881fe8ea
Michael Stallings <stall84@gmail.com>
2022-11-27 22:11:44 -0500

    Finished the handler, next to write the tf code

--- handler.ts
+++ handler.ts
@@ -1,3 +1,5 @@
+import { Handler } from 'aws-lambda'
+
 /**
  * @description Defines a very basic Lambda handler which uses the utility functions defined here to return a response to the requestor based on the queryParams present on the request uri
  *              Original credit and thanks to Elvis Ciotti for creating the project that can be found here: https://elvisciotti.medium.com/lambda-function-on-aws-with-terraform-2a38f3053a06
@@ -31,7 +33,8 @@ const QUOTE_ENTRIES = [
     '1: If you are not ashamed of what you were 1 year ago, then you have not improved much',
     '2: Success is determined by how well you manage failures',
     '3: The egg has a perfect shape, even if it\'s laid from the ass',
-    '4: Those who know everything learn nothing'
+    '4: Those who know everything learn nothing',
+    '5: Fuggit, Let\'s go to Amsterdam for New Years'
 ];
 
 // TODO: Get types for aws lambda and remove the 'any' typing
@@ -48,6 +51,68 @@ function successResponse(event: any, data: any): any {
     };
 }
 
-module.exports.handler = async (event: any) => {
-// Placeholder  
+function handleError(event: any, context: any): any {
+
+    const eventString = JSON.stringify({
+        event
+    })
+    const contextString = JSON.stringify({
+        context
+    })
+
+    return {
+        statusCode: 404,
+        body: contextString,
+        events: eventString 
+    }
+}
+
+interface ResponseObject {
+    statusCode: number
+    body: string
+}
+
+// module.exports.handler = async (event: any): Handler<ResponseObject> => {
+
+//     const action = event.queryStringParameters.action;
+//     const limit = event.queryStringParameters.limit ? parseInt(event.queryStringParameters.limit) : 10;
+
+//     if (action) {
+//         switch (action) {
+//             case 'random-quote':
+//                 const gratitudeItems = shuffle(QUOTE_ENTRIES);
+//                 return successResponse(event, gratitudeItems.slice(0, limit))
+//             case 'todays-date':
+//                 return successResponse(event, new Date())
+//             default:
+//                 return {
+//                     statusCode: 404,
+//                     body: "That action is not implemented"
+//                 }
+//         }
+//     }
+    
+// }
+
+export const handler: Handler<ResponseObject> = async (event: any, context: any) => {
+
+    const action = event.queryStringParameters.action;
+    const limit = event.queryStringParameters.limit ? parseInt(event.queryStringParameters.limit) : 10;
+
+    if (action) {
+        switch (action) {
+            case 'random-quote':
+                const gratitudeItems = shuffle(QUOTE_ENTRIES);
+                return successResponse(event, gratitudeItems.slice(0, limit))
+            case 'todays-date':
+                return successResponse(event, new Date())
+            default:
+                return {
+                    statusCode: 404,
+                    body: "That action is not implemented"
+                }
+        }
+    }
+    return handleError(event, context)
+    
 }
